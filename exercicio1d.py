0e5472126f4161aa7065a99e84d1f7eab0ef83ba
Luma Lynai <92832671+LumaLynai@users.noreply.github.com>
2022-03-28 23:55:58 -0300

    Add files via upload

---
+++ exercicio1d.py
@@ -0,0 +1,48 @@
+
+
+
+#1)
+n1 = int(input("Digite quatro digitos de 0 a 5: "))
+if n1 == 1234:
+    print("Parabéns! ")
+else:
+    print("Você errou")
+
+  #2)  
+nome1= str(input("Digite o seu nome? "))
+sobre1= str (input("sobrenome? "))
+nome2= str(input("Digite o seu nome? "))
+sobre2= str (input("sobrenome? "))
+nome3= str(input("Digite o seu nome? "))
+sobre3= str (input("sobrenome? "))
+print ("seu nome é:",nome1,'',sobre1)
+print('-----------')
+print("seu nome é:",nome2,'',sobre2)
+print('-----------')
+print("seu nome é:",nome3,'',sobre3)
+'''
+#3)     ñ sei como colocar uma lista de estados, nñ consegui rodar 
+estado = input("Digite a sigla do seu estado em letra maiuscula: ")
+if estado == SP:
+    print("O seu estado é São Paulo")
+elif estado == RJ:
+    print ("O seu estado é Rio de Janeiro")
+elif estado == BA:
+    print("O seu estado é Bahia")
+else:
+    print("Então eu não sei ")
+'''
+#4)
+n1=float(input("Digite um numero: "))
+n2=float(input("Digite outro numero: "))
+pote1=n1**2
+pote2=n2**3
+print("O primeiro numero elevado a dois é ",pote1,'O segundo elevado a três',pote2)
+
+#5) ?????ixiiii
+
+#6) 
+produto= str(input("Cadastre o nome do produto: "))
+descricao= str(input("Digite a descrição: "))
+valor=float(input("Digite o valor: "))
+print("Produto: ",produto,"  Descrição: ",descricao," valor R$",valor )
